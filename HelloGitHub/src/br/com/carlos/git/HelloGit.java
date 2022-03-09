e90f076b1b6aa8d3b89903c456a8d313fc322388
SilvaCP <79268743+SilvaCP@users.noreply.github.com>
2022-03-08 23:24:39 -0300

    Arquivo arrastado!
    Arrastei

---
+++ HelloGitHub/src/br/com/carlos/git/HelloGit.java
@@ -0,0 +1,15 @@
+/**
+ * 
+ */
+package br.com.carlos.git;
+
+/**
+ * @author cp
+ * Exemplo de integração do Eclipse com o github
+ *
+ */
+public class HelloGit {
+	public static void main(String[]args) {
+		System.out.println("Hello GitHub!");
+}
+}
